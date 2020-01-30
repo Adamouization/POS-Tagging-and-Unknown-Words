6626c8d1097a983976493101d32384b84979438f
Adam Jaamour <adam@jamour.com>
2020-01-30 09:23:40 +0000

    Base

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world")
+from nltk.corpus import brown
+
+import nltk
+nltk.download('brown')
+
+sents = brown.tagged_sents(tagset='universal')
+first = sents[0]
+print(first)
+words = [w for (w, _) in first]
+print(words)
+tags = [t for (_, t) in first]
+print(tags)
+
+
+def show_sent(sent):
+    print(sent)
+
+
+for sent in sents[0:10]:
+    show_sent(sent)
