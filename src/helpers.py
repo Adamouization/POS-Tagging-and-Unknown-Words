6fec8295082611d388f51e6cb365ac3833017b55
Adamouization <adam@jamour.com>
2020-02-26 18:09:16 +0000

    Reformat

---
+++ src/helpers.py
@@ -0,0 +1,7 @@
+def download_brown_corpus():
+    """
+    Download the Brown corpus and store a version locally.
+    :return: None.
+    """
+    import nltk
+    nltk.download('brown')
--- src/main.py
+++ src/main.py
@@ -1,20 +1,29 @@
 from nltk.corpus import brown
 
-import nltk
-nltk.download('brown')
-
-sents = brown.tagged_sents(tagset='universal')
-first = sents[0]
-print(first)
-words = [w for (w, _) in first]
-print(words)
-tags = [t for (_, t) in first]
-print(tags)
+from src.helpers import download_brown_corpus
 
 
-def show_sent(sent):
-    print(sent)
+def main():
+    sentence = brown.tagged_sents(tagset='universal')
+    first = sentence[0]
+    print("\nfirst")
+    print(first)
+
+    words = [w for (w, _) in first]
+    print("\nwords")
+    print(words)
+
+    tags = [t for (_, t) in first]
+    print("\ntags")
+    print(tags)
+    for s in sentence[0:10]:
+        show_sent(s)
 
 
-for sent in sents[0:10]:
-    show_sent(sent)
+def show_sent(sentence):
+    print("\nsentence")
+    print(sentence)
+
+
+if __name__ == "__main__":
+    main()
