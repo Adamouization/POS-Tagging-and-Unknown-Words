daf30dc348dfc5aead37e0595c65438c2f3b8ede
Adam Jaamour <adam@jamour.com>
2020-02-27 11:27:53 +0000

    Counting occurences of one POS tag following another

--- src/helpers.py
+++ src/helpers.py
@@ -5,3 +5,8 @@ def download_brown_corpus():
     """
     import nltk
     nltk.download('brown')
+
+
+def print_corpus_information(corpus):
+    print("Number of words in Brown corpus = {}".format(len(corpus.words())))
+    print("Number of sentences in Brown corpus = {}".format(len(corpus.tagged_sents(tagset='universal'))))
--- src/main.py
+++ src/main.py
@@ -1,30 +1,32 @@
 from nltk.corpus import brown
 
-from src.helpers import download_brown_corpus
+from src.helpers import download_brown_corpus, print_corpus_information
 
 
 def main():
-    print("Number of words in Brown corpus = {}".format(len(brown.words())))
+    counting_occ_pos = dict()
+    for sentence in brown.tagged_sents(tagset='universal'):
+        tags = [t for (_, t) in sentence]
+        current_tag, previous_tag = (str(),) * 2
 
-    sentence = brown.tagged_sents(tagset='universal')
-    first = sentence[0]
-    print("\nfirst")
-    print(first)
+        for i, tag in enumerate(tags):
+            # Special case: first tagged token does not have any preceding token.
+            if i == 0:
+                current_tag = tag
+            else:
+                previous_tag = current_tag
+                current_tag = tag
 
-    words = [w for (w, _) in first]
-    print("\nwords")
-    print(words)
+                # Create a new nested current tag dict in the dictionary.
+                if current_tag not in counting_occ_pos.keys():
+                    counting_occ_pos[current_tag] = dict()
 
-    tags = [t for (_, t) in first]
-    print("\ntags")
-    print(tags)
-    for s in sentence[0:10]:
-        show_sent(s)
+                # Create a new preceding tag value in the nested dict.
+                if previous_tag not in counting_occ_pos[current_tag].keys():
+                    counting_occ_pos[current_tag][previous_tag] = 0
 
-
-def show_sent(sentence):
-    print("\nsentence")
-    print(sentence)
+                # Increment the occurence once we are sure that the keys are correctly created in the dict.
+                counting_occ_pos[current_tag][previous_tag] += 1
 
 
 if __name__ == "__main__":
