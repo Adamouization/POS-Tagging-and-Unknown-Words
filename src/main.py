0b0b5621a305e331548082b118e6ef8cea368d3f
Adam Jaamour <adam@jamour.com>
2020-02-27 11:35:46 +0000

    Refactoring

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.helpers import download_brown_corpus, print_corpus_information
 
 
 def main():
-    counting_occ_pos = dict()
+    tag_transition_occurrences = dict()
     for sentence in brown.tagged_sents(tagset='universal'):
         tags = [t for (_, t) in sentence]
         current_tag, previous_tag = (str(),) * 2
@@ -18,15 +18,15 @@ def main():
                 current_tag = tag
 
                 # Create a new nested current tag dict in the dictionary.
-                if current_tag not in counting_occ_pos.keys():
-                    counting_occ_pos[current_tag] = dict()
+                if current_tag not in tag_transition_occurrences.keys():
+                    tag_transition_occurrences[current_tag] = dict()
 
                 # Create a new preceding tag value in the nested dict.
-                if previous_tag not in counting_occ_pos[current_tag].keys():
-                    counting_occ_pos[current_tag][previous_tag] = 0
+                if previous_tag not in tag_transition_occurrences[current_tag].keys():
+                    tag_transition_occurrences[current_tag][previous_tag] = 0
 
-                # Increment the occurence once we are sure that the keys are correctly created in the dict.
-                counting_occ_pos[current_tag][previous_tag] += 1
+                # Increment the occurrence once we are sure that the keys are correctly created in the dict.
+                tag_transition_occurrences[current_tag][previous_tag] += 1
 
 
 if __name__ == "__main__":
